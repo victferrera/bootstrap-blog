96edd99146d8b69610fc355b167ba788039f3fb9
Vitor <victor-bash2@hotmail.com>
2021-11-01 18:53:39 -0300

    Criado layout do blog

---
+++ Controllers/BlogController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApp.Controllers
+{
+    [Route("blog")]
+    public class BlogController : Controller
+    {
+        [Route("index")]
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("about")]
+        [HttpGet]
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        [Route("contact")]
+        [HttpGet]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
