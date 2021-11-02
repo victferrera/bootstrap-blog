bdbc2b2ffd4918467214b57f57332a34c07de995
Vitor <victor-bash2@hotmail.com>
2021-11-01 21:44:55 -0300

    Corrigindo layout painel

---
+++ Controllers/AutorController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApp.Controllers
+{
+    [Route("autor")]
+    public class AutorController : Controller
+    {
+        [Route("novo")]
+        [HttpGet]
+        public IActionResult Criar()
+        {
+            return View();
+        }
+    }
+}
