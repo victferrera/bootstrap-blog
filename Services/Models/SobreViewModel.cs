2ecb2fe6a8f792ffb5f50497800ca3c99cb2b829
Vitor Ferreira <victor-bash2@hotmail.com>
2021-11-05 14:54:02 -0300

    Começando implementação do CRUD da página Sobre

---
+++ Services/Models/SobreViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Services.Models
+{
+    class SobreViewModel
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public string Conteudo { get; set; }
+        public DateTime DataCriacao { get; set; }
+        public DateTime DataUltimaAlteracao { get; set; }
+    }
+}
