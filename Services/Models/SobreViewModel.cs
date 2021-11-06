5442ef8605d03e93dd89e56c49f07320cd30c245
Vitor <victor-bash2@hotmail.com>
2021-11-05 22:35:36 -0300

    Edições na página para salvar descrição e criado interface ISobreService

--- Services/Models/SobreViewModel.cs
+++ Services/Models/SobreViewModel.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Services.Models
 {
     public class SobreViewModel
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Campo {0} obrigatório")]
+        [DisplayName("Descrição")]
         public string Descricao { get; set; }
+        [Required(ErrorMessage = "Campo {0} obrigatório")]
+        [DisplayName("Conteúdo")]
         public string Conteudo { get; set; }
+        [DisplayName("Salvar como descrição principal")]
         public char StatusAtivo { get; set; }
         public DateTime DataCriacao { get; set; }
         public DateTime DataUltimaAlteracao { get; set; }
